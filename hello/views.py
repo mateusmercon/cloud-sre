b1acba60cdce34b04d10666f96bc64c9f7e1db75
Mateus Merçon <mateusmercon@gmail.com>
2024-06-25 10:03:46 -0300

    Alterar título da página

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 def home(request):
     html_content = """
     <html>
-    <head><title>Meu Perfil</title></head>
+    <head><title>Cloud&SRE </title></head>
     <body>
         <h1>Mateus Neves Merçon</h1>
         <h2>Cloud Computing & Site Reliability Engineering</h2>
